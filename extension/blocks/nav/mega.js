58e6fff21b61ed7b57ad91fb718b2eb9ba6cd054
Eric Bollens <ebollens@ucla.edu>
2013-11-09 18:05:31 -0800

    Update search and add utility helpers for margins and others

--- extension/blocks/nav/mega.js
+++ extension/blocks/nav/mega.js
@@ -52,7 +52,7 @@ WebBlocks.navMegaMenu = function(){
             base = navEle.children('ul');
         
         if(visible.length) // if open sub-menu, set height as sum of sub-menu and top-level menu
-            $(navEle).css('height', (visible.outerHeight()+base.outerHeight()+10)+'px')
+            $(navEle).css('height', (visible.outerHeight()+base.outerHeight())+'px')
         else // if no sub-menu open, set height to height of top-level menu
             $(navEle).css('height', base.outerHeight()+'px')
 
